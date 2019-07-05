5fec12b8fef8e926d74c4676f571e28caef14fd7
Eetu Tuomala <eetu.tuomala@maas.global>
2019-07-05 14:02:23 +0300

    allow to deploy with account id only, set deployment stages in order

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 'use strict';
 
 const AWS = require('aws-sdk');
+const { filter, isNil, pipe, head, sortBy } = require('ramda');
+
+function stageType(stage) {
+  if (/^\/.*\/(i|g|m|s|u|y)*$/.test(stage)) {
+    return 1;
+  } else if (isNil(stage)) {
+    return 2;
+  }
+  return 0;
+}
 
 function testStage(stage, deploymentDefinitionStage) {
+  if (isNil(deploymentDefinitionStage)) {
+    return true;
+  }
   let regexp;
-  if (/^\/.*\/(i|g|m|s|u|y)*$/.test(deploymentDefinitionStage)) {
+  if (stageType(deploymentDefinitionStage) === 1) {
     const pattern = deploymentDefinitionStage.substr(1, deploymentDefinitionStage.lastIndexOf('/') - 1);
     const flag = deploymentDefinitionStage.substr(deploymentDefinitionStage.lastIndexOf('/') + 1);
     regexp = new RegExp(pattern, flag);
@@ -44,27 +57,20 @@ class DeploymentManagerPlugin {
       throw new Error('[serverless-deployment-guard] service.custom.deployment definition is missing');
     }
 
-    const deploymentDefinitions = service.custom.deployment.filter(({ stage }) =>
-      testStage(processedInput.options.stage, stage)
-    );
+    const deploymentDefinition = pipe(
+      filter(({ stage }) => testStage(processedInput.options.stage, stage)),
+      sortBy(({ stage }) => stageType(stage)),
+      head
+    )(service.custom.deployment);
 
-    if (deploymentDefinitions.length === 0) {
+    if (isNil(deploymentDefinition)) {
       throw new Error(`[serverless-deployment-guard] stage '${processedInput.options.stage}' cannot be deployed`);
     }
 
-    if (deploymentDefinitions.every(item => !!item.accountId)) {
+    if (!isNil(deploymentDefinition.accountId)) {
       const sts = new AWS.STS({ region: processedInput.options.region });
       const { Account } = await sts.getCallerIdentity().promise();
-
-      const exists = !!service.custom.deployment.find(item => {
-        return (
-          item.stage &&
-          item.accountId &&
-          testStage(processedInput.options.stage, item.stage) &&
-          item.accountId.toString() === Account
-        );
-      });
-      if (!exists) {
+      if (deploymentDefinition.accountId !== Account) {
         throw new Error(
           `[serverless-deployment-guard] stage '${processedInput.options.stage}' cannot be deployed to account '${Account}'`
         );
--- test/index.test.js
+++ test/index.test.js
@@ -60,6 +60,19 @@ describe('#authorizer handler', () => {
     await expect(deploymentManager.validateStageAndAccount()).resolves.toBeUndefined();
   });
 
+  it('should pass if account is correct', async () => {
+    const deploymentManager = new DeploymentGuardPlugin({
+      processedInput: { options: { stage: 'dev' } },
+      service: {
+        custom: {
+          deployment: [{ accountId: '0123456789' }],
+        },
+      },
+    });
+
+    await expect(deploymentManager.validateStageAndAccount()).resolves.toBeUndefined();
+  });
+
   it('should pass if regexp stage with flags and account is correct', async () => {
     const deploymentManager = new DeploymentGuardPlugin({
       processedInput: { options: { stage: 'SANDBOX' } },
@@ -73,18 +86,19 @@ describe('#authorizer handler', () => {
     await expect(deploymentManager.validateStageAndAccount()).resolves.toBeUndefined();
   });
 
-  it('should pass if stage and account is correct in one of the definitions', async () => {
-    const deploymentManager = new DeploymentGuardPlugin({
-      processedInput: { options: { stage: 'dev' } },
-      service: {
-        custom: {
-          deployment: [{ stage: 'dev', accountId: '1234567890' }, { stage: 'dev', accountId: '0123456789' }],
-        },
-      },
-    });
+  // TODO: later
+  // it('should pass if stage and account is correct in one of the definitions', async () => {
+  //   const deploymentManager = new DeploymentGuardPlugin({
+  //     processedInput: { options: { stage: 'dev' } },
+  //     service: {
+  //       custom: {
+  //         deployment: [{ stage: 'dev', accountId: '1234567890' }, { stage: 'dev', accountId: '0123456789' }],
+  //       },
+  //     },
+  //   });
 
-    await expect(deploymentManager.validateStageAndAccount()).resolves.toBeUndefined();
-  });
+  //   await expect(deploymentManager.validateStageAndAccount()).resolves.toBeUndefined();
+  // });
 
   it('should pass if stage is correct and no account id is defined', async () => {
     const deploymentManager = new DeploymentGuardPlugin({
@@ -105,6 +119,24 @@ describe('#authorizer handler', () => {
       service: {
         custom: {
           deployment: [{ stage: 'dev', accountId: '1234567890' }],
+        },
+      },
+    });
+    await expect(deploymentManager.validateStageAndAccount()).rejects.toThrow({
+      message: "[serverless-deployment-guard] stage 'dev' cannot be deployed to account '0123456789'",
+    });
+  });
+
+  it('should throw error if trying to deploy to incorrect account', async () => {
+    const deploymentManager = new DeploymentGuardPlugin({
+      processedInput: { options: { stage: 'dev' } },
+      service: {
+        custom: {
+          deployment: [
+            { accountId: '0123456789' },
+            { stage: 'dev', accountId: '1234567890' },
+            { stage: '/ev$/i', accountId: '1234567890' },
+          ],
         },
       },
     });
