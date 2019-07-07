0ebb48921cd49b7f61265223ac97d2f629296b74
Eetu Tuomala <eetu.tuomala@maas.global>
2019-07-07 16:11:39 +0300

    wait for npm to propagate the version

--- scripts/tag.sh
+++ scripts/tag.sh
@@ -3,6 +3,7 @@
 # if master branch and not a pull request
 if [ $TRAVIS_BRANCH == 'master' ] && [ -z $TRAVIS_PULL_REQUEST_BRANCH ]
 then
+  sleep 5
   latest=$(npm info serverless-deployment-manager dist-tags.latest)
   git tag v${latest}
   git push https://maasglobal:$GITHUB_API_TOKEN@github.com/maasglobal/serverless-deployment-manager v${latest}
